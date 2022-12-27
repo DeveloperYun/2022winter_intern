6f4d562f5e577f8ee11dd1e019a419ebba8869b9
DeveloperYun <yhb7588@naver.com>
2022-12-27 11:10:36 +0900

    allowed all

--- ajinmain/settings.py
+++ ajinmain/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-t-awqy3gfwzz+$zoqciwq+5x%ic+(*b7b2yy#x*-@xt98-vti-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
